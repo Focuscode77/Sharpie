6f160021a4d82f86beffb27ead7d217b6d58ee61
Joshua Coleman <50680485+Focuscode77@users.noreply.github.com>
2024-11-09 15:13:44 -0500

    Update  time.js
    Javascript refresh

--- time.js
+++ time.js
@@ -11,15 +11,77 @@ let mind = {
 
     }
 }
-
-console.log(mind.team.version)
-
-
-
-function Real(a, b) {
-
-    console.log(a + b)
+//  Frist Class Function Objects
+function greet() {
+    console.log('hi')
 
 }
 
-Real(10, 4)
+var anonymousGreet = () => {
+
+    console.log('hey')
+}
+
+anonymousGreet()
+
+function log(a) {
+
+    a();
+
+}
+
+log(function () {
+
+    console.log('hi')
+
+});
+
+//By value 
+var a = 3;
+var b;
+b = a;
+
+console.log(a);
+console.log(b);
+
+// by reference
+
+var c = { greeting: 'hi' };
+var d;
+
+d = c;
+c.greeting = 'hello' //mutate
+console.log(c);
+console.log(d);
+
+//equals operartor sets up new memory space
+
+c = { greeting: 'howdy' };
+//this points to the window  object as a defualt 
+function r() {
+    console.log(this);
+
+}
+
+var k = function () {
+
+    console.log(this)
+
+}
+
+k();
+
+r();
+
+
+// this  points  to the object
+var t = {
+
+    name: 'the t obect',
+
+    log: function () {
+        console.log(this)
+    }
+}
+
+t.log();
