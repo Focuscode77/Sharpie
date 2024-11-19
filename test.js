84d46abf1c66703705dcdfc520ddfc844a893dd4
Joshua Coleman <50680485+Focuscode77@users.noreply.github.com>
2024-11-19 18:19:23 -0500

    Test files added
    More javascript pratices

---
+++ test.js
@@ -0,0 +1,20 @@
+console.log('I am in the console');
+
+function greet(hello) {
+
+    if ("test" === hello) {
+
+        console.log('variable  is  a  string')
+
+
+    } else {
+
+
+        console.log('try again')
+
+    }
+
+
+}
+
+greet('test')
