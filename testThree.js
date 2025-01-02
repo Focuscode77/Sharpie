4ba61b5903dd8ba1b25d54ff4b34d2f776f468b3
Joshua Coleman <50680485+Focuscode77@users.noreply.github.com>
2025-01-02 10:24:21 -0500

    Update testThree.js
    Working on JSON Object

--- testThree.js
+++ testThree.js
@@ -7,3 +7,44 @@ const Greeting = (hello) => {
 };
 
 Greeting('hello');
+
+// Understand JSON Objects
+
+const simpleObject = { key: 'valueOne', key2: 'valueTwo', key3: 'valueThree' };
+
+const simpleArray = ['One', 'Two', 'Three'];
+
+const arrayOfObjects = [
+  { key: 'valueOne' },
+  { key2: 'valueTwo' },
+  { key3: 'valueThree' },
+];
+
+const typesOfData = { string: 'this is a string', number: 5 };
+
+const mix = {
+  FirstName: 'Arthur',
+  LastName: 'Test',
+  Age: 18,
+  Student: [
+    {
+      FirstName: 'Dan',
+      LastName: 'Jim',
+    },
+    {
+      FirstName: 'Sam',
+      LastName: 'Time',
+    },
+  ],
+};
+
+console.log(simpleObject.key);
+console.log(simpleArray[0]);
+console.log(arrayOfObjects[0]);
+console.log(typesOfData.number);
+console.log(mix.Age);
+console.log(mix.Student[0].FirstName);
+
+const newStudent = mix.Student[0].FirstName;
+
+console.log(newStudent);
