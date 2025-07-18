e78de93cfddec7096d9ee30cff49f505d4104fd2
Joshua Coleman <50680485+Focuscode77@users.noreply.github.com>
2025-07-17 20:10:53 -0400

    Renewing

---
+++ javascript/app.js
@@ -0,0 +1,14 @@
+// This is a comment in JavaScript
+console.log('Hello I  am in  the connsole');
+
+const timearray = ['Tim', 'Dan', 'Sam'];
+
+const newObject = {
+  nameOne: timearray[0],
+  nameTwo: timearray[1],
+  nameThree: timearray[2],
+};
+
+console.log(newObject.nameOne);
+console.log(newObject.nameTwo);
+console.log(newObject.nameThree);
